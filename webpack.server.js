0226d303c941a22b41db2910f25f2ee2c8f1ea99
Taha Uygun <tahauygun@gmail.com>
2018-12-03 23:52:15 -0600

    setting up webpack

--- webpack.server.js
+++ webpack.server.js
@@ -13,6 +13,9 @@ module.exports = {
     // Tell webpack where to put the output file
     // that is generated
 
-
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    }
 
 }
