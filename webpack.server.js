a9a3028752719287f075680d1d346ce2ce8548a2
Taha Uygun <tahauygun@gmail.com>
2018-12-03 23:51:46 -0600

    setting up webpack

--- webpack.server.js
+++ webpack.server.js
@@ -3,4 +3,16 @@ module.exports = {
     // for nodeJS, rather than for the browser
 
     target: 'node',
+
+    // Tell webpack the root file of our
+    // server application
+
+    entry: './src/index.js',
+
+
+    // Tell webpack where to put the output file
+    // that is generated
+
+
+
 }
