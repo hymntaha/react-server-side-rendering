d0d62b98759767595defd955e0c2dc466780e74b
Taha Uygun <tahauygun@gmail.com>
2018-12-04 23:24:00 -0600

    webpack merge

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+
+const config = {
     // Inform webpack that we are building a bundle
     // for nodeJS, rather than for the browser
 
@@ -18,24 +21,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    },
-
-    // Tell webpack to run babel on every file it runs through
-    module: {
-        rule: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets:[
-                        'react',
-                        'stage-0',
-                        ['env',{ targets: { browsers:['last 2 version'] }}]
-                    ]
-                }
-            }
-        ]
     }
-
 }
+
+module.exports = merge(baseConfig, config);
